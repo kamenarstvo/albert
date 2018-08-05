bac6d910b68c4f9df6a0167ce17d2766f85f3027
Gregor Albert <gregoralbert@gmail.com>
2018-08-05 08:31:29 +0200

    header update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 
 import {AppComponent} from './app.component';
 import {ContactComponent} from './contact/contact.component';
 import {HomeComponent} from './home/home.component';
-import {AppRoutingModule} from './/app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AgmCoreModule} from '@agm/core';
 import {OfferComponent} from './offer/offer.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderMenuComponent} from './header-menu/header-menu.component';
 
 
 @NgModule({
